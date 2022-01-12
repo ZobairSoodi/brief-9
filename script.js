a8d687a061c926d423432a1d908374d528088d68
ZobairSoodi <soodi.zobair.solicode@gmail.com>
2022-01-12 23:05:32 +0100

    add by region feature

--- script.js
+++ script.js
@@ -1,22 +1,60 @@
 var main = $("#content");
+var continent = "Africa";
 
 async function getCountries(){
-    var response = await fetch("https://restcountries.com/v3.1/region/africa")
+    var response = await fetch("https://restcountries.com/v3.1/region/"+continent)
     var data = response.json();
+    console.log(data)
     return data;
 }
 
-getCountries().then(data => {
-    var rand = Math.floor(Math.random()*40);
-    for(var i = rand; i < rand + 8; i++){
-        var image = data[i]["flags"]["png"];
-        var name_c = data[i]["name"]["common"];
-        var capital = data[i]["capital"][0];
-        var population = data[i]["population"]
-        var div = document.createElement("div");
-        div.setAttribute("class", "col-xs-12 col-sm-6 col-md-4 col-lg-3 thumbnail");
-        div.innerHTML = `<img src="${image}"><h3>${name_c} <span class="h5 text-muted">(${population})</span></h3><h5 class="text-light">${capital}</h5>`;
-        main.append(div);
-    }
-})
+function printCountries(){
+    getCountries().then(data => {
 
+        var rand = Math.floor(Math.random()*data.length-8);
+        if(rand<0){
+            rand = 0;
+        }
+        
+        for(var i = rand; i < rand + 8; i++){
+            
+            if(data[i].flags){
+                var image = data[i]["flags"]["png"];
+            }
+            else{
+                image = "sIaRmaFSMfrw8QJIBAa8mA-article.png"
+            }
+            var name_c = data[i]["name"]["common"];
+            if(name_c.length>10){
+                name_c = name_c.substring(0,10);
+                name_c+="...";
+            }
+            
+            if(data[i]["capital"]){
+                var capital = data[i]["capital"][0];
+            }
+            else{
+                capital = "none"
+            }
+            var population = data[i]["population"]
+            var div = document.createElement("div");
+            div.setAttribute("class", "col-xs-12 col-sm-6 col-md-4 col-lg-3 thumbnail");
+            div.innerHTML = `<img src="${image}"><h3>${name_c} <span class="h5 text-muted">(${population})</span></h3><h5 class="text-light">${capital}</h5>`;
+            main.append(div);
+        }
+    })
+}
+printCountries()
+
+$(".dropdown-menu a").click(function(){
+    $("#content").html("");
+    var sel = $(this).text()
+    switch (sel){
+        case "Asia": continent="Asia";break;
+        case "Africa": continent="Africa";break;
+        case "America": continent="America";break;
+        case "Europe": continent="Europe";break;
+    }
+    
+    printCountries();
+})
